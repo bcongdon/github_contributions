0b09c694ac51416db6f84110e0691165f9943df1
Benjamin Congdon <bencongdon96@gmail.com>
2017-07-15 10:32:17 -0700

    Further work on API

--- github_contributions/contributions.py
+++ github_contributions/contributions.py
@@ -1,10 +1,33 @@
+from collections import namedtuple
 from datetime import date
+from dateutil.parser import parse
+
+from .utils import level_for_fill
+
+
+Day = namedtuple('Day', ['date', 'count', 'level'])
+
+
+def _parse_soup(soup):
+    day_elems = soup.find_all('rect', class_='day')
+
+    days = [Day(date=parse(x['data-date']).date(),
+                count=int(x['data-count']),
+                level=level_for_fill(x['fill']))
+            for x in day_elems]
+    return days
 
 
 class GithubContributions(object):
-    def __init__(self, days, end_date):
-        self.days = days
-        self.end_date = end_date
+    def __init__(self, soup=None, days=None):
+        if days:
+            self.days = days
+        else:
+            self.days = _parse_soup(soup)
+        self._streaks = None
+
+    def _filter_start_date(self, start_date):
+        self.days = [day for day in self.days if day.date >= start_date]
 
     def today(self):
         try:
@@ -13,4 +36,26 @@ class GithubContributions(object):
             raise RuntimeError('No contribution data found for today')
 
     def streaks(self):
-        pass
+        if self._streaks:
+            return self._streaks
+
+        streaks = []
+        current_streak = []
+        for day in self.days:
+            if day.count == 0 and current_streak:
+                streaks.append(current_streak)
+                current_streak = []
+            elif day.count > 0:
+                current_streak.append(day)
+        if current_streak:
+            streaks.append(current_streak)
+
+        self._streaks = streaks
+        return streaks
+
+    def __str__(self):
+        template = '<GithubContributions {0} days of data ending at {1}>'
+        return template.format(len(self.days), self.end_date)
+
+    def __repr__(self):
+        return self.__str__()
--- github_contributions/user.py
+++ github_contributions/user.py
@@ -1,42 +1,96 @@
-from collections import namedtuple
+from datetime import timedelta, date
 
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import requests
 
 from .contributions import GithubContributions
-from .utils import level_for_fill
 
 BASE_URL = 'https://github.com'
-CONTRIB_URL = BASE_URL + '/users/{}/contributions'
-
-
-Day = namedtuple('Day', ['date', 'count', 'level'])
+CONTRIB_URL = BASE_URL + '/users/{0}/contributions'
 
 
 class GithubUser(object):
     def __init__(self, username, url=CONTRIB_URL):
         self._username = username
         self._url = url
+        self._current_data = None
 
-    def contributions(self, end_date=None):
-        params = {'from': end_date} if end_date else None
+    def _get_contributions(self, from_date):
+        # Return cached data if applicable
+        if from_date == date.today() and self._current_data:
+            return self._current_data
+
+        params = {'from': from_date} if from_date != date.today() else {}
         try:
             url = self._url.format(self._username)
             req = requests.get(url, params=params)
             svg = req.content
             soup = BeautifulSoup(svg, 'html.parser')
         except Exception as ex:
-            raise RuntimeError('Unable to get Github Data: {}'.format(ex))
+            raise RuntimeError('Unable to get Github Data: {0}'.format(ex))
 
-        day_elems = soup.find_all('rect', class_='day')
-        days = [Day(date=parse(x['data-date']).date(),
-                    count=int(x['data-count']),
-                    level=level_for_fill(x['fill']))
-                for x in day_elems]
-        end_date = parse(end_date).date() if end_date else day_elems[-1].date
+        contributions = GithubContributions(soup=soup)
 
-        return GithubContributions(days=days, end_date=end_date)
+        if from_date == date.today():
+            self._current_data = contributions
+
+        return contributions
+
+    def contributions(self, start_date=None, end_date=None):
+        '''
+        Fetches the contribution history for the given user
+
+        Returns a GithubContributions object
+        '''
+
+        start_date = parse(str(start_date)).date() if start_date else None
+        end_date = parse(str(end_date)).date() if end_date else None
+
+        # Need to set an end_date to call Github API
+        if start_date and not end_date:
+            # Default end_date to be ~1 year ahead of start_date
+            end_date = start_date + timedelta(days=365)
+        elif not end_date:
+            # Default end_date to today
+            end_date = date.today()
+
+        contributions = self._get_contributions(end_date)
+
+        # Filter by start_date if necessary
+        if start_date:
+            contributions._filter_start_date(start_date)
+
+        return contributions
+
+    def longest_streak(self):
+        streaks = self.contributions().streaks()
+        max_streak = max(streaks, key=lambda s: len(s))  # pylint: disable=unnecessary-lambda
+        if len(max_streak) < 365:
+            return max_streak
+        return self.current_streak()
+
+    def _get_multi_year_streak(self, curr_start, curr_streak=None):
+        curr_streak = curr_streak or []
+        prev_end_date = curr_start - timedelta(days=1)
+        prev_contribs = self.contributions(end_date=prev_end_date)
+        last_streak_start = prev_contribs.streaks()[-1][0].date
+        prev_contribs_start = prev_contribs.days[0].date
+        if last_streak_start != prev_contribs_start:
+            return prev_contribs.streaks()[-1] + curr_streak
+
+        combined_streak = prev_contribs.streaks()[-1] + curr_streak
+        return self._get_multi_year_streak(prev_contribs_start,
+                                           combined_streak)
 
     def current_streak(self):
-        pass
+        contributions = self.contributions()
+        known_streaks = contributions.streaks()
+        if not known_streaks:
+            return known_streaks
+        elif len(known_streaks[-1]) < 365:
+            return known_streaks[-1]
+
+        start_date = contributions.days[0].date
+        curr_streak = known_streaks[-1]
+        return self._get_multi_year_streak(start_date, curr_streak)
