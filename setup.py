ab58a09ad550b5cabe8f52dd179e0f719fa644f1
Benjamin Congdon <bencongdon96@gmail.com>
2019-06-08 08:43:04 -0700

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 from setuptools import setup
 
 setup(name='GithubContributions',
-      version='0.1.2',
+      version='0.1.3',
       description='A Python interface for Github\'s user contribution system',
       author='Benjamin Congdon',
       author_email='me@bcon.gdn',
       url='https://github.com/bcongdon/github_contributions',
       packages=['github_contributions'],
       install_requires=[
-        'requests',
-        'bs4',
-        'python-dateutil'
+          'requests',
+          'bs4',
+          'python-dateutil'
       ],
       extras_require={
-        'dev': [
-          'pytest',
-          'responses==0.5.1',
-          'mock'
-        ]
+          'dev': [
+              'pytest',
+              'responses==0.5.1',
+              'mock'
+          ]
       })
